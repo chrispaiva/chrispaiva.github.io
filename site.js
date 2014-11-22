a9f6d4100516fcc7e342aef9f5bda0a8055e8f7a
Chris Paiva <chrispaivamail@geemail.com>
2014-11-22 13:20:14 +1100

    portfolio section and some animations

--- site.js
+++ site.js
@@ -1,10 +1,18 @@
 $(document).ready(function($) {
 	var controller = new ScrollMagic();
+	var tween;
 
 	new ScrollScene({offset: 0})
-					.setPin("#pin1", {pushFollowers: false})
-					.addTo(controller);
+		.setPin("#pin1")
+		.addTo(controller);
 
-	// show indicators (requires debug extension)
-	scene.addIndicators();
+	tween = TweenMax.staggerFrom(".item1 ul li", 2, {scale:0.5, opacity:0, delay:0.5, ease:Elastic.easeOut, force3D:true}, 0.2);
+	new ScrollScene({offset: 100, duration: 700})
+		.setTween(tween)
+		.addTo(controller);
+
+	tween = TweenMax.staggerFrom(".item2 ul li", 2, {scale:0.5, opacity:0, delay:0.5, ease:Elastic.easeOut, force3D:true}, 0.2);
+	new ScrollScene({offset: 400, duration: 700})
+		.setTween(tween)
+		.addTo(controller);
 });
